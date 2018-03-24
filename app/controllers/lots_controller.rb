d7f9eff3fb9b7aefc680e40a0c335da36462956c
Mitchell Walters <mitchellwalters@Mitchells-MacBook-Pro.local>
2018-03-23 19:16:41 -0700

    hard code update is working... now permit the params...

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -3,6 +3,9 @@ class LotsController < ApplicationController
   end
 
   def edit_helper
+    params.permit!
+    current_lot = Lot.find(1)
+    current_lot.update({available_spaces: 2})
   end
 
 end

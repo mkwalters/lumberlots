68ad07b0a436aa9adbed82fec4fb056ed5e7f6e6
Mitchell Walters <mitchellwalters@Mitchells-MacBook-Pro.local>
2018-03-24 14:00:54 -0700

    ya we actually want the schools lots...

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,7 +2,7 @@ class SchoolsController < ApplicationController
   def get_lots
   	params.permit!
 
-  	lots = School.where(name: params[:name])
+  	lots = School.where(name: params[:name])[0].lots
 
   	render :json => {"lots": lots}
 

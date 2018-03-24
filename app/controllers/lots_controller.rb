76a7f9a6251cb640bf868504bf1b4fe53bad993a
Mitchell Walters <mitchellwalters@Mitchells-MacBook-Pro.local>
2018-03-23 20:56:58 -0700

    show

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -7,5 +7,11 @@ class LotsController < ApplicationController
     current_lot = Lot.find(params[:id])
     current_lot.update({car_count: params[:qty]})
   end
+  
+  def show
+    params.permit!
+    current_lot = Lot.find(params[:id])
+    render :json => {'status':'stoked', 'car_count': current_lot.car_count, 'total_spaces': current_lot.total_spaces}
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'lot_update/:id/qty/:qty', to: 'lots#edit_helper'  
+  get 'lots/:id', to: 'lots#show'
 end
